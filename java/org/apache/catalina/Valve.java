99b80fe48b6161e491258a4c757a969be14b9cfd
tianfeng <tianfengsp>
2018-03-25 01:41:01 +0800

    add

--- java/org/apache/catalina/Valve.java
+++ java/org/apache/catalina/Valve.java
@@ -28,7 +28,7 @@ import org.apache.catalina.connector.Response;
  * particular Container.  A series of Valves are generally associated with
  * each other into a Pipeline.  The detailed contract for a Valve is included
  * in the description of the <code>invoke()</code> method below.</p>
- *
+ * <p>
  * <b>HISTORICAL NOTE</b>:  The "Valve" name was assigned to this concept
  * because a valve is what you use in a real world pipeline to control and/or
  * modify flows through it.
@@ -45,7 +45,7 @@ public interface Valve {
     /**
      * @return the next Valve in the pipeline containing this Valve, if any.
      */
-    public Valve getNext();
+    Valve getNext();
 
 
     /**
@@ -53,7 +53,7 @@ public interface Valve {
      *
      * @param valve The new next valve, or <code>null</code> if none
      */
-    public void setNext(Valve valve);
+    void setNext(Valve valve);
 
 
     //---------------------------------------------------------- Public Methods
@@ -64,59 +64,58 @@ public interface Valve {
      * invoked inside the classloading context of this container. Unexpected
      * throwables will be caught and logged.
      */
-    public void backgroundProcess();
+    void backgroundProcess();
 
 
     /**
      * <p>Perform request processing as required by this Valve.</p>
-     *
+     * <p>
      * <p>An individual Valve <b>MAY</b> perform the following actions, in
      * the specified order:</p>
      * <ul>
      * <li>Examine and/or modify the properties of the specified Request and
-     *     Response.
+     * Response.
      * <li>Examine the properties of the specified Request, completely generate
-     *     the corresponding Response, and return control to the caller.
+     * the corresponding Response, and return control to the caller.
      * <li>Examine the properties of the specified Request and Response, wrap
-     *     either or both of these objects to supplement their functionality,
-     *     and pass them on.
+     * either or both of these objects to supplement their functionality,
+     * and pass them on.
      * <li>If the corresponding Response was not generated (and control was not
-     *     returned, call the next Valve in the pipeline (if there is one) by
-     *     executing <code>getNext().invoke()</code>.
+     * returned, call the next Valve in the pipeline (if there is one) by
+     * executing <code>getNext().invoke()</code>.
      * <li>Examine, but not modify, the properties of the resulting Response
-     *     (which was created by a subsequently invoked Valve or Container).
+     * (which was created by a subsequently invoked Valve or Container).
      * </ul>
-     *
+     * <p>
      * <p>A Valve <b>MUST NOT</b> do any of the following things:</p>
      * <ul>
      * <li>Change request properties that have already been used to direct
-     *     the flow of processing control for this request (for instance,
-     *     trying to change the virtual host to which a Request should be
-     *     sent from a pipeline attached to a Host or Context in the
-     *     standard implementation).
+     * the flow of processing control for this request (for instance,
+     * trying to change the virtual host to which a Request should be
+     * sent from a pipeline attached to a Host or Context in the
+     * standard implementation).
      * <li>Create a completed Response <strong>AND</strong> pass this
-     *     Request and Response on to the next Valve in the pipeline.
+     * Request and Response on to the next Valve in the pipeline.
      * <li>Consume bytes from the input stream associated with the Request,
-     *     unless it is completely generating the response, or wrapping the
-     *     request before passing it on.
+     * unless it is completely generating the response, or wrapping the
+     * request before passing it on.
      * <li>Modify the HTTP headers included with the Response after the
-     *     <code>getNext().invoke()</code> method has returned.
+     * <code>getNext().invoke()</code> method has returned.
      * <li>Perform any actions on the output stream associated with the
-     *     specified Response after the <code>getNext().invoke()</code> method has
-     *     returned.
+     * specified Response after the <code>getNext().invoke()</code> method has
+     * returned.
      * </ul>
      *
-     * @param request The servlet request to be processed
+     * @param request  The servlet request to be processed
      * @param response The servlet response to be created
-     *
-     * @exception IOException if an input/output error occurs, or is thrown
-     *  by a subsequently invoked Valve, Filter, or Servlet
-     * @exception ServletException if a servlet error occurs, or is thrown
-     *  by a subsequently invoked Valve, Filter, or Servlet
+     * @throws IOException      if an input/output error occurs, or is thrown
+     *                          by a subsequently invoked Valve, Filter, or Servlet
+     * @throws ServletException if a servlet error occurs, or is thrown
+     *                          by a subsequently invoked Valve, Filter, or Servlet
      */
-    public void invoke(Request request, Response response)
-        throws IOException, ServletException;
+    void invoke(Request request, Response response)
+            throws IOException, ServletException;
 
 
-    public boolean isAsyncSupported();
+    boolean isAsyncSupported();
 }
