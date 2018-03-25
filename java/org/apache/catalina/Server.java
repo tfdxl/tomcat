f935e8dc33a876ff6dc893718e8a3ca641198ec2
tianfeng <tianfengsp>
2018-03-25 10:50:26 +0800

    add

--- java/org/apache/catalina/Server.java
+++ java/org/apache/catalina/Server.java
@@ -53,7 +53,7 @@ public interface Server extends Lifecycle {
     /**
      * @return the global naming resources.
      */
-    public NamingResourcesImpl getGlobalNamingResources();
+    NamingResourcesImpl getGlobalNamingResources();
 
 
     /**
@@ -61,20 +61,20 @@ public interface Server extends Lifecycle {
      *
      * @param globalNamingResources The new global naming resources
      */
-    public void setGlobalNamingResources
-        (NamingResourcesImpl globalNamingResources);
+    void setGlobalNamingResources
+    (NamingResourcesImpl globalNamingResources);
 
 
     /**
      * @return the global naming resources context.
      */
-    public javax.naming.Context getGlobalNamingContext();
+    javax.naming.Context getGlobalNamingContext();
 
 
     /**
      * @return the port number we listen to for shutdown commands.
      */
-    public int getPort();
+    int getPort();
 
 
     /**
@@ -82,13 +82,13 @@ public interface Server extends Lifecycle {
      *
      * @param port The new port number
      */
-    public void setPort(int port);
+    void setPort(int port);
 
 
     /**
      * @return the address on which we listen to for shutdown commands.
      */
-    public String getAddress();
+    String getAddress();
 
 
     /**
@@ -96,13 +96,13 @@ public interface Server extends Lifecycle {
      *
      * @param address The new address
      */
-    public void setAddress(String address);
+    void setAddress(String address);
 
 
     /**
      * @return the shutdown command string we are waiting for.
      */
-    public String getShutdown();
+    String getShutdown();
 
 
     /**
@@ -110,7 +110,7 @@ public interface Server extends Lifecycle {
      *
      * @param shutdown The new shutdown command
      */
-    public void setShutdown(String shutdown);
+    void setShutdown(String shutdown);
 
 
     /**
@@ -118,7 +118,7 @@ public interface Server extends Lifecycle {
      * {@link #getCatalina()} {@link Catalina#getParentClassLoader()}. If
      * catalina has not been set, return the system class loader.
      */
-    public ClassLoader getParentClassLoader();
+    ClassLoader getParentClassLoader();
 
 
     /**
@@ -126,20 +126,20 @@ public interface Server extends Lifecycle {
      *
      * @param parent The new parent class loader
      */
-    public void setParentClassLoader(ClassLoader parent);
+    void setParentClassLoader(ClassLoader parent);
 
 
     /**
      * @return the outer Catalina startup/shutdown component if present.
      */
-    public Catalina getCatalina();
+    Catalina getCatalina();
 
     /**
      * Set the outer Catalina startup/shutdown component if present.
      *
      * @param catalina the outer Catalina component
      */
-    public void setCatalina(Catalina catalina);
+    void setCatalina(Catalina catalina);
 
 
     /**
@@ -147,7 +147,7 @@ public interface Server extends Lifecycle {
      * may be the same (and are by default). If this is not set the value
      * returned by {@link #getCatalinaHome()} will be used.
      */
-    public File getCatalinaBase();
+    File getCatalinaBase();
 
     /**
      * Set the configured base (instance) directory. Note that home and base
@@ -155,14 +155,14 @@ public interface Server extends Lifecycle {
      *
      * @param catalinaBase the configured base directory
      */
-    public void setCatalinaBase(File catalinaBase);
+    void setCatalinaBase(File catalinaBase);
 
 
     /**
      * @return the configured home (binary) directory. Note that home and base
      * may be the same (and are by default).
      */
-    public File getCatalinaHome();
+    File getCatalinaHome();
 
     /**
      * Set the configured home (binary) directory. Note that home and base
@@ -170,7 +170,7 @@ public interface Server extends Lifecycle {
      *
      * @param catalinaHome the configured home directory
      */
-    public void setCatalinaHome(File catalinaHome);
+    void setCatalinaHome(File catalinaHome);
 
 
     // --------------------------------------------------------- Public Methods
@@ -181,13 +181,14 @@ public interface Server extends Lifecycle {
      *
      * @param service The Service to be added
      */
-    public void addService(Service service);
+    void addService(Service service);
 
 
     /**
+     * 一直等到shutdown的命令
      * Wait until a proper shutdown command is received, then return.
      */
-    public void await();
+    void await();
 
 
     /**
@@ -196,13 +197,13 @@ public interface Server extends Lifecycle {
      * @param name Name of the Service to be returned
      * @return the specified Service, or <code>null</code> if none exists.
      */
-    public Service findService(String name);
+    Service findService(String name);
 
 
     /**
      * @return the set of Services defined within this Server.
      */
-    public Service[] findServices();
+    Service[] findServices();
 
 
     /**
@@ -211,12 +212,12 @@ public interface Server extends Lifecycle {
      *
      * @param service The Service to be removed
      */
-    public void removeService(Service service);
+    void removeService(Service service);
 
 
     /**
      * @return the token necessary for operations on the associated JNDI naming
      * context.
      */
-    public Object getNamingToken();
+    Object getNamingToken();
 }
